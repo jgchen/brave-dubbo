ecf35c852ae5c2be622b7700185e645410f76604
陈贵 <chengui@reapal.com>
2017-03-03 10:34:09 +0800

    添加http-bravejar包

--- src/main/java/com/github/kristofa/brave/dubbo/BraveFactoryBean.java
+++ src/main/java/com/github/kristofa/brave/dubbo/BraveFactoryBean.java
@@ -1,24 +1,20 @@
 package com.github.kristofa.brave.dubbo;
 
-import com.github.kristofa.brave.Brave;
-import com.github.kristofa.brave.LoggingSpanCollector;
-import com.github.kristofa.brave.Sampler;
-import com.github.kristofa.brave.SpanCollector;
+import com.github.kristofa.brave.*;
+import com.github.kristofa.brave.http.HttpSpanCollector;
+import org.springframework.beans.factory.BeanInitializationException;
 import org.springframework.beans.factory.FactoryBean;
 
 /**
  * Created by jack-cooper on 2017/2/20.
  */
 public class BraveFactoryBean implements FactoryBean<Brave> {
+    /**服务名*/
     private String serviceName;
-
-    private SpanCollector spanCollector = new LoggingSpanCollector();
-
-    private Sampler sampler = Sampler.create(1.0f);
-
-    public void setSampler(Sampler sampler) {
-        this.sampler = sampler;
-    }
+    /**zipkin服务器ip及端口，不配置默认打印日志*/
+    private String zipkinHost;
+    /**单例模式*/
+    private Brave instance;
 
     public void setServiceName(String serviceName) {
         this.serviceName = serviceName;
@@ -28,13 +24,35 @@ public class BraveFactoryBean implements FactoryBean<Brave> {
         return serviceName;
     }
 
-    public void setSpanCollector(SpanCollector spanCollector) {
-        this.spanCollector = spanCollector;
+    public String getZipkinHost() {
+        return zipkinHost;
     }
+
+    public void setZipkinHost(String zipkinHost) {
+        this.zipkinHost = zipkinHost;
+    }
+
+    private void createInstance() {
+        if (this.serviceName == null) {
+            throw new BeanInitializationException("Property serviceName must be set.");
+        }
+        Brave.Builder builder = new Brave.Builder(this.serviceName);
+        if (this.zipkinHost != null && !"".equals(this.zipkinHost)) {
+            builder.spanCollector(HttpSpanCollector.create(this.zipkinHost, new EmptySpanCollectorMetricsHandler()));
+        }else{
+            builder.spanCollector(new LoggingSpanCollector()).traceSampler(Sampler.create(1.0f)).build();
+        }
+        this.instance = builder.build();
+    }
+
+
 
     @Override
     public Brave getObject() throws Exception {
-        return new Brave.Builder(serviceName).spanCollector(spanCollector).traceSampler(sampler).build();
+        if (this.instance == null) {
+            this.createInstance();
+        }
+        return this.instance;
     }
 
     @Override
