e6f2c7a6b90d2dac3135eb68c23d90c5cb4fb026
陈贵 <chengui@reapal.com>
2017-03-13 17:53:57 +0800

    bug

--- src/main/java/com/github/kristofa/brave/dubbo/support/DefaultServerNameProvider.java
+++ src/main/java/com/github/kristofa/brave/dubbo/support/DefaultServerNameProvider.java
@@ -12,11 +12,6 @@ import com.github.kristofa.brave.dubbo.DubboServerNameProvider;
  *   dubbo默认没有提供,只能标准化项目包名
  *   形如 com.reapal.appname.serverName
  */
-/**
- *   解析dubbo Provider applicationName
- - *   dubbo默认没有提供,只能标准化项目包名
- - *   形如 com.company.bu.serverName.api.XXXX
- */
 public class DefaultServerNameProvider implements DubboServerNameProvider {
     @Override
     public String resolveServerName(RpcContext rpcContext) {
